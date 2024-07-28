65592d40a0653dfd0524033dbd86bf92a17f1110
NICK B <nickbean2255@yahoo.com>
2024-07-28 01:51:26 -0400

    update

--- client.build/compile.sh
+++ client.build/compile.sh
@@ -10,4 +10,7 @@ rm script.py
 python setup.py build_ext --inplace
 cp src/script.py ./
 # pyarmor gen --pack FC -r script.py
-pyarmor gen --pack FC -r script.py --include-requirements=src/requirements.txt
+# pyarmor gen --pack FC -r script.py --include-requirements=requirements.txt
+# pyarmor gen --pack FC -r script.py --runtime-dir requirements.txt
+
+py onefile.py
