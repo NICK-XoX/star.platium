997901040721674317b7664bfe00679a8a8cb62f
NICK B <nickbean2255@yahoo.com>
2024-07-28 01:12:26 -0400

    update

--- client.build/compile.sh
+++ client.build/compile.sh
@@ -2,10 +2,10 @@
 
 source src/venv/bin/activate
 pip install -r src/requirements.txt
-# rm -rf build
-# rm *cpython*
-# rm src/*.c
-# rm *.so
-# rm script.py
-# python setup.py build_ext --inplace
-# cp src/script.py ./
+rm -rf build
+rm *cpython*
+rm src/*.c
+rm *.so
+rm script.py
+python setup.py build_ext --inplace
+cp src/script.py ./
